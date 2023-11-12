0e1a392e254b1c1e2e2f9078f2762a7d56499ea4
johneliasj <jonsonelias@outlook.com>
2023-11-11 21:24:56 -0300

    quinta aula

---
+++ conversao.py
@@ -0,0 +1,23 @@
+# CONVERSÃO DE TIPOS
+
+idade = "26"
+numero1 = "10"
+numero2 = "20"
+print(numero1 + numero2)
+
+print(idade, type(idade))
+
+idade_inteira = int(idade) #int
+print(idade_inteira, type(idade_inteira)) 
+"""
+Existem:
+
+int()
+bool()
+float()
+str()
+
+"""
+
+altura = float(input('Informe a sua altura: '))
+print(altura, type(altura))
